b8271ef6755f4c592477da51df79f389a7e4fe19
bw2015 <38880080+bw2015@users.noreply.github.com>
2021-08-10 18:25:44 +0800

    订阅队列设置为空，将会随机生成

--- Web.Pusher/Caching/PushConsumer.cs
+++ Web.Pusher/Caching/PushConsumer.cs
@@ -6,6 +6,7 @@ using RabbitMQ.Client.Events;
 using SP.StudioCore.Configuration;
 using SP.StudioCore.MQ;
 using SP.StudioCore.MQ.RabbitMQ;
+using SP.StudioCore.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +14,12 @@ using System.Threading.Tasks;
 
 namespace Web.Pusher.Caching
 {
-    [Consumer(Name = "Connection", ExchangeType = ExchangeType.fanout, ExchangeName = MessageExchangeName.MESSAGE, QueueName = "Local")]
+    [Consumer(Name = "Connection", ExchangeType = ExchangeType.fanout, ExchangeName = MessageExchangeName.MESSAGE)]
     public class PushConsumer : IListenerMessage
     {
         public bool Consumer(string message, object sender, BasicDeliverEventArgs ea)
         {
+            ConsoleHelper.WriteLine($"收到消息：{message}", ConsoleColor.Blue);
             MessageModel model = JsonConvert.DeserializeObject<MessageModel>(message);
             PushService.SendAsync(model).Wait();
             return true;
